3f15d7f9a8a93db7c67c255ad199472f7d51aae6
dirqaharaka <54818623+dirkaharaka99@users.noreply.github.com>
2021-10-09 01:22:53 -0700

    Add files via upload

---
+++ Day49_TodoList/script.js
@@ -0,0 +1,8 @@
+const form = document.getElementsById("form");
+const input = document.getElementsById("input");
+const todosUL = document.getElementsById("todos");
+
+form.addEventListener('submit', (e) => {
+    e.preven
+})
+
